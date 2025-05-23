bb210c6609c8fd2d9b49d329afa8f7b542183273
lsl233 <ahands233@gmail.com>
2025-05-23 20:03:16 +0800

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,15 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@astrojs/react';
 
 
+import sitemap from '@astrojs/sitemap';
+
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://toolify100.com',
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
